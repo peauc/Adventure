e11dcf51847fb30be7f5453996011838cf7e7111
Peau Clement <clement.peau@epitech.eu>
2016-04-11 18:59:21 +0200

    Here comes the makefile

--- menu/src_menu/menu_load.c
+++ menu/src_menu/menu_load.c
@@ -5,16 +5,19 @@
 ** Login   <peau_c@epitech.net>
 **
 ** Started on  Mon Apr 11 18:14:27 2016 Poc
-** Last update Mon Apr 11 18:41:54 2016 Poc
+** Last update Mon Apr 11 18:54:23 2016 Poc
 */
 
 #include <stdlib.h>
 #include "default.h"
 
-int	load_menu(t_menu **menu)
+int			load_menu(t_menu **menu)
 {
+  t_bunny_pixelarray	*sprite;
+
   if ((*menu = malloc(sizeof(t_menu))) == NULL)
     return (1);
   if (((*menu)->buttons = malloc(sizeof(t_button) * 5)) == NULL)
     return (1);
+  if (sprite = bunny_load_pixelarray("sprites/give.png"))
 }
