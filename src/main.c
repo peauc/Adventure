3a869f322bb5779d2ee89166b967db5e4c1d1797
Peau Clement <clement.peau@epitech.eu>
2016-04-17 21:54:05 +0200

    Push

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login   <peau_c@epitech.net>
 **
 ** Started on  Thu Nov 19 10:13:25 2015 clement peau
-** Last update Sun Apr 17 21:16:08 2016 marel_m
+** Last update Sun Apr 17 21:51:49 2016 Poc
 */
 
 #include "tekadv.h"
@@ -73,47 +73,52 @@ t_sprite_sheet          *load_sprite_sheet(char *sprite_sheet,
   return (sp);
 }
 
+int			initialize(t_data *data)
+{
+  if ((data->win = bunny_start(WIDTH, HEIGHT, false, "test")) == NULL)
+    return (1);
+  if ((load_all_scene(data) == -1) ||
+      (data->pixel = bunny_new_pixelarray(WIDTH, HEIGHT)) == NULL ||
+      (data->new = bunny_new_pixelarray(WIDTH, HEIGHT)) == NULL ||
+      (data->menu = load_menu()) == NULL ||
+      (data->mv_s = malloc(sizeof(t_mv_scene))) == NULL ||
+      (load_scene_tab(data->tab)) ||
+      (load_music()))
+      return (1);
+  data->mv_s->s_nb = 0;
+  data->mv_s->mv_bck = 0;
+  data->mv_s->mv_fr = 0;
+  data->mv_s->click = 0;
+  bunny_set_key_response((t_bunny_key)&escape);
+  bunny_set_click_response((t_bunny_click)&clicky);
+  if ((data->player = bunny_malloc(sizeof(t_player))) == NULL
+      || (data->player->sp =
+	  load_sprite_sheet("sprites/sprite_monkey_island.png",
+			    pos_(125, 290), 10)) == NULL
+      || (data->player->pix =
+	  bunny_new_pixelarray(data->player->sp->size.x,
+			       data->player->sp->size.y)) == NULL)
+    return (1);
+  return (0);
+}
+
 int			main()
 {
   t_data		data;
 
   bunny_set_maximum_ram(10000000000);
-  if (load_all_scene(&data) == -1)
-      return (1);
-  data.win = bunny_start(WIDTH, HEIGHT, false, "test");
-  if ((data.pixel = bunny_new_pixelarray(WIDTH, HEIGHT)) == NULL)
+  if (initialize(&data))
     return (1);
-  if ((data.new = bunny_new_pixelarray(WIDTH, HEIGHT)) == NULL)
-      return (1);
-  if ((data.menu = load_menu()) == NULL)
-      return (1);
-  if ((data.mv_s = malloc(sizeof(t_mv_scene))) == NULL)
-      return (1);
-  if ((load_scene_tab(data.tab)))
-      return (1);
-  if ((load_music()))
+  if ((data.p = create_list()) == NULL)
     return (1);
-  if ((data.player = bunny_malloc(sizeof(t_player))) == NULL
-      || (data.player->sp = load_sprite_sheet("sprites/sprite_monkey_island.png",
-					      pos_(125, 290), 10)) == NULL
-      || (data.player->pix = bunny_new_pixelarray(data.player->sp->size.x,
-						  data.player->sp->size.y)) == NULL)
-    return (0);
-  data.p = create_list();
   if ((full_ini(data.p, "pictures/harbor_back.png")) == -1)
-    return (0);
+    return (1);
   data.node = change_list(data.p);
   pos_player(data.player, data.node);
-  data.mv_s->s_nb = 0;
-  data.mv_s->mv_bck = 0;
-  data.mv_s->mv_fr = 0;
-  data.mv_s->click = 0;
   if ((data.p = change_road(0, data.p)) == NULL
       || (data.node = change_list(data.p)) == NULL)
     return (1);
   bunny_set_loop_main_function((t_bunny_loop)mainloop);
-  bunny_set_key_response((t_bunny_key)&escape);
-  bunny_set_click_response((t_bunny_click)&clicky);
   if (bunny_loop(data.win, 60, &data) == 0)
     return (0);
   load_music();
